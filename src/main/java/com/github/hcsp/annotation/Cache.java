3f9ad8124f7cfada983d5648cca834cbe4310565
Hana <384073877@qq.com>
2019-12-12 19:29:57 +0800

    基于注解的缓存装饰器

--- src/main/java/com/github/hcsp/annotation/Cache.java
+++ src/main/java/com/github/hcsp/annotation/Cache.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)//运行时也可以通过反射被扫描
 public @interface Cache {
     // 标记缓存的时长（秒），默认60s
     int cacheSeconds() default 60;
