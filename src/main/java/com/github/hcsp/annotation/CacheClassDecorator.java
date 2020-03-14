78a5cb6c9023647252aa817eef252e434589bfdc
KisetsuLee <lizhijie_lee@126.com>
2020-03-14 17:23:18 +0800

    注解解析类

--- src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
+++ src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
@@ -1,14 +1,35 @@
 package com.github.hcsp.annotation;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
+import net.bytebuddy.implementation.bind.annotation.Origin;
+import net.bytebuddy.implementation.bind.annotation.RuntimeType;
+import net.bytebuddy.implementation.bind.annotation.This;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CacheClassDecorator {
+    private static ConcurrentHashMap cacheMap = new ConcurrentHashMap();
+
     // 将传入的服务类Class进行增强
     // 使得返回一个具有如下功能的Class：
     // 如果某个方法标注了@Cache注解，则返回值能够被自动缓存注解所指定的时长
     // 这意味着，在短时间内调用同一个服务的同一个@Cache方法两次
     // 它实际上只被调用一次，第二次的结果直接从缓存中获取
     // 注意，缓存的实现需要是线程安全的
+    @SuppressWarnings("unchecked")
     public static <T> Class<T> decorate(Class<T> klass) {
-        return klass;
+        return (Class<T>) new ByteBuddy()
+                .subclass(klass)
+                .method(ElementMatchers.isAnnotatedWith(Cache.class))
+                .intercept(MethodDelegation.to(CacheInterceptor.class))
+                .make()
+                .load(klass.getClassLoader())
+                .getLoaded();
     }
 
     public static void main(String[] args) throws Exception {
@@ -23,5 +44,17 @@ public class CacheClassDecorator {
         System.out.println(dataService.queryDataWithoutCache(1));
         Thread.sleep(1 * 1000);
         System.out.println(dataService.queryDataWithoutCache(1));
+    }
+
+    static class CacheInterceptor {
+        @RuntimeType
+        public static Object cache(@Origin Method method,
+                                   @This Object obj,
+                                   @AllArguments Object[] arguments) {
+            System.out.println(method.getName());
+            System.out.println(obj);
+            System.out.println(Arrays.toString(arguments));
+            return null;
+        }
     }
 }
