f07a808140cc3f0a5c37b34fc287c3e5ea1de0f3
cyunt <chenyuntao1997@163.com>
2020-08-28 19:29:06 +0800

    实现AOP缓存

--- src/main/java/com/github/hcsp/annotation/Cache.java
+++ src/main/java/com/github/hcsp/annotation/Cache.java
@@ -1,5 +1,10 @@
 package com.github.hcsp.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+//设置在运行时保留注解
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Cache {
     // 标记缓存的时长（秒），默认60s
     int cacheSeconds() default 60;
