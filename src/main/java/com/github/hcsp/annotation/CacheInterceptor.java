e4103e570ea87de3c73c4f1ce72bd6010e3fd760
bowen-wu <13793248162@163.com>
2021-08-03 20:17:18 +0800

    feat: use @AllArguments Object[] arguments

--- src/main/java/com/github/hcsp/annotation/CacheInterceptor.java
+++ src/main/java/com/github/hcsp/annotation/CacheInterceptor.java
@@ -1,30 +1,32 @@
 package com.github.hcsp.annotation;
 
+import net.bytebuddy.implementation.bind.annotation.AllArguments;
 import net.bytebuddy.implementation.bind.annotation.Origin;
 import net.bytebuddy.implementation.bind.annotation.SuperCall;
 
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 public class CacheInterceptor {
     private static Long previousTime = null;
-    private static Parameter[] previousParameters;
+    private static Object[] previousArguments;
     private static List<Object> memoryDatabase;
 
 
-    public static List<Object> getCacheDataOrRequery(@SuperCall Callable<List<Object>> zuper, @Origin Method method) {
+    public static List<Object> getCacheDataOrRequery(
+            @SuperCall Callable<List<Object>> zuper,
+            @Origin Method method,
+            @AllArguments Object[] arguments
+    ) {
         try {
             int cacheSeconds = method.getAnnotation(Cache.class).cacheSeconds();
-            Parameter[] parameters = method.getParameters();
-            if (!Arrays.equals(parameters, previousParameters) || null == memoryDatabase || (previousTime != null && System.currentTimeMillis() - previousTime > cacheSeconds * 1000L)) {
-                System.out.println("call!!!!!");
+            if (!Arrays.equals(arguments, previousArguments) || null == memoryDatabase || (previousTime != null && System.currentTimeMillis() - previousTime > cacheSeconds * 1000L)) {
                 memoryDatabase = zuper.call();
             }
 
-            previousParameters = parameters;
+            previousArguments = arguments;
             previousTime = System.currentTimeMillis();
             return memoryDatabase;
         } catch (Exception e) {
