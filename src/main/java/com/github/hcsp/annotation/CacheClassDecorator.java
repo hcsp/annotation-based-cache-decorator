548752629e72a73c5bd609544e100a8532b0ac14
bowen-wu <13793248162@163.com>
2021-08-03 20:07:05 +0800

    feat: parameters is error equals

--- src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
+++ src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
@@ -1,5 +1,11 @@
 package com.github.hcsp.annotation;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.method.MethodDescription;
+import net.bytebuddy.implementation.MethodDelegation;
+
+import java.util.stream.Stream;
+
 public class CacheClassDecorator {
     // 将传入的服务类Class进行增强
     // 使得返回一个具有如下功能的Class：
@@ -7,8 +13,21 @@ public class CacheClassDecorator {
     // 这意味着，在短时间内调用同一个服务的同一个@Cache方法两次
     // 它实际上只被调用一次，第二次的结果直接从缓存中获取
     // 注意，缓存的实现需要是线程安全的
-    public static <T> Class<T> decorate(Class<T> klass) {
-        return klass;
+    public static <T> Class<? extends T> decorate(Class<T> klass) {
+        return new ByteBuddy()
+                .subclass(klass)
+                .method(CacheClassDecorator::methodWithCacheAnnotation)
+                .intercept(MethodDelegation.to(CacheInterceptor.class))
+                .make()
+                .load(klass.getClassLoader())
+                .getLoaded();
+    }
+
+    private static boolean methodWithCacheAnnotation(MethodDescription methodDescription) {
+        int count = (int) Stream.of(methodDescription.getDeclaredAnnotations())
+                .filter(annotationDescriptions -> annotationDescriptions.isAnnotationPresent(Cache.class))
+                .count();
+        return count > 0;
     }
 
     public static void main(String[] args) throws Exception {
@@ -17,6 +36,8 @@ public class CacheClassDecorator {
         // 有缓存的查询：只有第一次执行了真正的查询操作，第二次从缓存中获取
         System.out.println(dataService.queryData(1));
         Thread.sleep(1 * 1000);
+        System.out.println(dataService.queryData(1));
+        Thread.sleep(5 * 1000);
         System.out.println(dataService.queryData(1));
 
         // 无缓存的查询：两次都执行了真正的查询操作
