4a4a233dd383a6f40e58c774d3261c6ec94efdea
jianqi <jianqi.zhao.xian@gmail.com>
2021-10-21 00:09:54 +1100

    job done

--- src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
+++ src/main/java/com/github/hcsp/annotation/CacheClassDecorator.java
@@ -1,5 +1,16 @@
 package com.github.hcsp.annotation;
 
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.*;
+import net.bytebuddy.matcher.ElementMatchers;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class CacheClassDecorator {
     // 将传入的服务类Class进行增强
     // 使得返回一个具有如下功能的Class：
@@ -8,7 +19,56 @@ public class CacheClassDecorator {
     // 它实际上只被调用一次，第二次的结果直接从缓存中获取
     // 注意，缓存的实现需要是线程安全的
     public static <T> Class<T> decorate(Class<T> klass) {
-        return klass;
+        return (Class<T>) new ByteBuddy().subclass(klass).method(ElementMatchers.isAnnotatedWith(Cache.class))
+                .intercept(MethodDelegation.to(CacheAdvisor.class))
+                .make().load(klass.getClassLoader()).getLoaded();
+    }
+
+    public static class CacheAdvisor {
+        private static final Map<CacheKey, CacheValue> cache = new ConcurrentHashMap<>();
+        @RuntimeType
+        public static List<Object> cache(
+                @Origin Method method,
+                @This Object thisObject,
+                @AllArguments Object[] arguments,
+                @SuperCall Callable<List<Object>> superCall
+                ) {
+            CacheKey cacheKey = new CacheKey(method, thisObject, arguments);
+            CacheValue result = cache.get(cacheKey);
+            if (resultAlreadyExists(result) && resultIsNotExpired(result, method)) {
+                return result.getValue();
+            } else {
+                return fetchResultByNewComputationThenUpdateCache(superCall, cacheKey);
+            }
+        }
+
+        private static List<Object> fetchResultByNewComputationThenUpdateCache(Callable<List<Object>> superCall, CacheKey cacheKey) {
+            List<Object> superCallResult;
+            try {
+                superCallResult = superCall.call();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            cache.put(cacheKey, new CacheValue(superCallResult, System.currentTimeMillis()));
+            return superCallResult;
+        }
+
+        private static boolean resultIsNotExpired(CacheValue result, Method method) {
+            if (result == null) {
+                return false;
+            } else {
+                int expireThreshold = method.getAnnotation(Cache.class).cacheSeconds();
+                return System.currentTimeMillis() - result.getTimeStamp() < expireThreshold * 1000L;
+            }
+        }
+
+        private static boolean resultAlreadyExists(CacheValue result) {
+            if (result == null) {
+                return false;
+            } else {
+                return cache.values().stream().anyMatch(cacheValue -> cacheValue.getValue().equals(result.getValue()));
+            }
+        }
     }
 
     public static void main(String[] args) throws Exception {
@@ -17,6 +77,8 @@ public class CacheClassDecorator {
         // 有缓存的查询：只有第一次执行了真正的查询操作，第二次从缓存中获取
         System.out.println(dataService.queryData(1));
         Thread.sleep(1 * 1000);
+        System.out.println(dataService.queryData(1));
+        Thread.sleep(5 * 1000);
         System.out.println(dataService.queryData(1));
 
         // 无缓存的查询：两次都执行了真正的查询操作
